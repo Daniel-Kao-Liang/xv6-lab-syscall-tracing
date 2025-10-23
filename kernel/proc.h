8bdc3f9a31b52f8f6c26025c0f0451ab4bf56d5a
daniel Liang <80109666+Daniel-Kao-Liang@users.noreply.github.com>
2025-10-23 06:48:18 +0000

    modified:   kernel/proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -105,3 +105,9 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
 };
+
+struct traced
+{
+  int syscall_num;
+  int count;
+};
